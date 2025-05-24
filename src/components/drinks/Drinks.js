80857e2e4c9edb18d05c8e4e837598dbca7a5bb8
Mayra <mayra.bf3@gmail.com>
2025-05-23 22:02:45 -0300

    version 2

--- src/components/drinks/Drinks.js
+++ src/components/drinks/Drinks.js
@@ -7,32 +7,26 @@ function Drinks() {
   const [cocktails, setCocktails] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  
-
-  const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita";
+  const url =
+    "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita";
 
   useEffect(() => {
     const fetchDrinks = async () => {
-      
-      try{
+      try {
         const response = await fetch(url);
         const data = await response.json();
 
         setCocktails(data.drinks.slice(0, 3));
         console.log(data.drinks);
-      }
-      catch(error){
+      } catch (error) {
         console.error("Error fetching drinks:", error);
-      }
-      finally{
+      } finally {
         setLoading(false);
       }
-      
     };
 
     fetchDrinks();
   }, []);
-    
 
   return (
     <div>
@@ -43,16 +37,16 @@ function Drinks() {
           <h2>Margaritas</h2>
           <ul className="drinks-ul">
             {cocktails?.map((drink) => (
-              
-                <li key={drink.idDrink}>
-                  <div className="drink-card">
-                    <h2>{drink.strDrink}</h2>
-                    <h3>{drink.strCategory}</h3>
+              <li key={drink.idDrink}>
+                <div className="drink-card">
+                  <div className="card-content">
+                    <h2>{drink.strDrink}</h2> 
+                    <h4>{drink.strCategory}</h4>
+                    <img src={drink.strDrinkThumb} />                
                     <p>{drink.strInstructions}</p>
-                    <img src={drink.strDrinkThumb}/>
                   </div>
-                </li>
-              
+                </div>
+              </li>
             ))}
           </ul>
         </div>
