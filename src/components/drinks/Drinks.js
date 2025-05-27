1e58f76bcf856041ec63986bd0a16a0e9989a47f
Mayra <mayra.bf3@gmail.com>
2025-05-27 15:05:56 -0300

    drinks updated

--- src/components/drinks/Drinks.js
+++ src/components/drinks/Drinks.js
@@ -4,20 +4,23 @@ import { useState, useEffect } from "react";
 
 // strCategory, strDrink: Nombre del trago, strGlass: vaso, strIngredient1-15, strInstructions
 function Drinks() {
-  const [cocktails, setCocktails] = useState([]);
+  const [vodkaCocktails, setVodkaCocktails] = useState([]);
+  const [ginCocktails, setGinCocktails] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  const url =
+  const urlVodka =
     "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=vodka";
 
+  const urlGin = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=gin";
+
   useEffect(() => {
-    const fetchDrinks = async () => {
+    const fetchVodkaDrinks = async () => {
       try {
-        const response = await fetch(url);
+        const response = await fetch(urlVodka);
         const data = await response.json();
 
-        setCocktails(data.drinks.slice(0, 3));
-        console.log(data.drinks);
+        setVodkaCocktails(data.drinks.slice(0, 3));
+        console.log(data.drinks.slice(0, 3));
       } catch (error) {
         console.error("Error fetching drinks:", error);
       } finally {
@@ -25,18 +28,39 @@ function Drinks() {
       }
     };
 
-    fetchDrinks();
+    fetchVodkaDrinks();
   }, []);
+
+  useEffect(() => {
+    const fetchGinDrinks = async () => {
+      try {
+        const response = await fetch(urlGin);
+        const data = await response.json();
+
+        setGinCocktails(data.drinks.slice(0, 3));
+        console.log(data.drinks.slice(0, 3));
+
+      }
+      catch (error){
+        console.error("Error fetching drinks:", error);
+      }
+      finally {
+        setLoading(false);
+      }
+    }
+
+    fetchGinDrinks();
+  }, [])
 
   return (
     <div>
       <h1 className="title">Some Drinks</h1>
 
       <div className="container">
-        <div className="margaritas">
-          <h2 id="container-subtitle">Vodka</h2>
+        <div className="drinks">
+          <h2 className="container-subtitle">Vodka</h2>
           <ul className="drinks-ul">
-            {cocktails?.map((drink) => (
+            {vodkaCocktails?.map((drink) => (
               <li key={drink.idDrink} className="drink-card">
                 <div >
                   <div className="card-titles">
@@ -45,8 +69,8 @@ function Drinks() {
                   </div>  
                   
                   <div className="drink-content">
-                    <p id="drink-description">{drink.strInstructions}</p>
-                    <img src={drink.strDrinkThumb} id="drink-image"/>                
+                    <p className="drink-description">{drink.strInstructions}</p>
+                    <img src={drink.strDrinkThumb} className="drink-image" alt="Drink image"/>                
                   </div>
 
                   
@@ -57,6 +81,31 @@ function Drinks() {
               </li>
             ))}
           </ul>
+
+          <h2 className="container-subtitle">Gin</h2>
+          
+          <ul className="drinks-ul">
+            {ginCocktails?.map((drink) => (
+              <li key={drink.idDrink} className="drink-card">
+                <div>
+                  <div className="card-titles">
+                    <h2>{drink.strDrink}</h2>
+                    <h4>{drink.strCategory}</h4>
+                  </div>
+
+                  <div className="drink-content">
+                    <p className="drink-description">{drink.strInstructions}</p>
+                    <img src={drink.strDrinkThumb} className="drink-image" alt="Drink image"/>
+                  </div>
+                </div>
+
+                <div className="more-btn">
+                  <button className="btn-more">More</button>
+                </div>
+              </li>
+            ))}
+          </ul>
+
         </div>
       </div>
     </div>
